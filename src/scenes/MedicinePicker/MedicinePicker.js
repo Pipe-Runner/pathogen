62c8a2064e067dec5b060183a8cc3f2ce78d0052
root <root@localhost.localdomain>
2018-01-27 01:48:51 +0530

    medicine picker input field added

--- src/scenes/MedicinePicker/MedicinePicker.js
+++ src/scenes/MedicinePicker/MedicinePicker.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Dropdown,Button } from 'semantic-ui-react'
+
+const medicineOptions = [{key:'aklakl',value: 'kjasjlajl',text:'jhdhkskhds'},{key:'aklakl',value: 'kjasjlajl',text:'jhdhkskhds'},{key:'aklakl',value: 'kjasjlajl',text:'jhdhkskhds'}]
 
 class MedicinePicker extends Component {
   constructor(props) {
@@ -7,7 +10,12 @@ class MedicinePicker extends Component {
   }
 
   render() {
-    return <div>medicine</div>;
+    return <div><Dropdown placeholder='Select Medicine' fluid search selection options={medicineOptions} />
+    <Button.Group>
+    <Button>un</Button>
+    <Button.Or text='ou' />
+    <Button positive>deux</Button>
+  </Button.Group></div>;
   }
 }
 
