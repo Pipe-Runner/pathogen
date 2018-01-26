3a18805cac84ce69f44b3f57bdd007cebb43431d
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-01-26 23:31:11 +0530

    google map working

--- src/scenes/Map/Map.js
+++ src/scenes/Map/Map.js
@@ -4,9 +4,15 @@ import MapRenderer from './components/MapRenderer';
 import { MapContainer } from './styles.Map.js';
 
 const Map = () => (
-  <MapContainer>
-    <MapRenderer isMarkerShown={true} />
-  </MapContainer>
+  <div>
+    <MapRenderer
+      isMarkerShown={true}
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBiNfMAYS471tn8hxoNkoaK-dZAfYyU1Gs&v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `100vh` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  </div>
 );
 
 export default Map;
